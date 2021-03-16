bb68d7726f98bcae0a305f822571078172f02d80
Andrés Arias Rauld <andresariasrauld@gmail.com>
2021-03-16 12:04:39 -0300

    adding some method to api user

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -68,8 +68,64 @@ updateUser = async (req, res) => {
     }
 }
 
+deleteUser = async (req, res) => {
+    await User.findOneAndDelete({ _id: req.params.id }, (err, user) => {
+        if(err){
+            return res
+                .status(400)
+                .json({ 
+                    success: false, 
+                    error: err 
+                })
+        }
+        if( !user ){
+            return res
+                .status(404)
+                .json({
+                    success: false,
+                    error: `User not found`
+                })
+        }
+
+        return res
+            .status(200)
+            .json({
+                sucess: true,
+                data: user
+            })
+    }).catch(err => console.log(err))
+}
+
+getUserById = async (req, res) => {
+    await User.findOne({ _id: req.params.id }, (err, user) => {
+        if( err ){
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    error: err
+                })
+        }
+        if( !user ){
+            return res
+                .status(404)
+                .json({
+                    success: false,
+                    error: `User not found`
+                })
+        }
+        return res
+            .status(200)
+            .json({
+                success: true,
+                data: user
+            })
+    }).catch(err => console.log(err))
+}
 
 module.exports = {
     createUser,
     getUsers,
+    deleteUser,
+    getUserById
 }
